875098ecb3ad29a1b50b01fe3c6f239a97472193
sophiebaud11 <38045114+sophiebaud11@users.noreply.github.com>
2020-04-22 12:59:47 -0400

    print comments

--- src/components/TextPane.js
+++ src/components/TextPane.js
@@ -9,27 +9,48 @@ export default function TextPane({ showComment, playBackOn, time }) {
   const counter = useRef(0)
   const [visible, setVisible] = useState([])
   const prevTime = useRef()
+  const [commentRoll, setCommentRoll] = useState([])
 
   useEffect(() => {
-    let sessions = []
-    if (playBackOn) {
-      firebase.database().ref(`/videos/${videoID}/sessions`).once('value').then(snapshot => {
-        const data = snapshot.val()
-        sessions = Object.keys(data)
-        sessions.forEach(id => {
-          const idRef = firebase.database().ref(`/sessions/${id}/comments`)
-          idRef.on('value', (snapshot) => {
-            console.log(snapshot.val())
-          })
+  let sessions = []
+  let commentArrays = []
+  if (playBackOn) {
+    firebase.database().ref(`/videos/${videoID}/sessions`).on('value', snapshot => {
+      const data = snapshot.val()
+      sessions = Object.keys(data)
+      sessions.forEach(id => {
+        const idRef = firebase.database().ref(`/sessions/${id}/comments`)
+        idRef.on('value', (snapshot) => {
+          console.log(snapshot.val())
+          commentArrays.push(snapshot.val())
+          if (snapshot.val()) {
+            var i
+            for (i = 0; i < snapshot.val().length; i++) {
+              console.log(snapshot.val()[i].text)
+              const commentToAdd = snapshot.val()[i].text
+              const tsToAdd = snapshot.val()[i].ts
+              console.log(commentToAdd)
+              setCommentRoll(commentRoll => [...commentRoll, {text: commentToAdd, ts: tsToAdd}])
+            }
+          }
         })
+        return commentArrays
       })
-    }
-    return () => {sessions.forEach(id => {
-      const idRef = firebase.database().ref(`/sessions/${id}/comments`)
-      idRef.off()
-    })}
-  }, [playBackOn, videoID])
+    })
+  }
+  return () => {
+    firebase.database().ref(`/videos/${videoID}/sessions`).off()
+    sessions.forEach(id => {
+    const idRef = firebase.database().ref(`/sessions/${id}/comments`)
+    idRef.off()
+  })}
+}, [playBackOn, videoID])
+  console.log(commentRoll)
 
+  const items = commentRoll.map((array, i) => {
+      return <p key={i}>{array.text} {array.ts}</p>
+    })
+  console.log(items)
   // useEffect(() => {
   //   if (playBackOn && (time < prevTime.current)) {
   //     const nowVisible = comments.filter(comment => comment.ts <= time)
@@ -43,10 +64,20 @@ export default function TextPane({ showComment, playBackOn, time }) {
   //   }
   // }, [time, comments, playBackOn, setVisible])
 
-  if (playBackOn) {
-    return visible.map(comment => <p style={{marginLeft: '5%'}}>[{comment.ts}] {comment.text}</p>)
-  }
+//   if (playBackOn) {
+//       return commentRoll.map(comment => <p style={{marginLeft: '5%'}}>[{comment.ts}] {comment.text}</p>)
+//     }
+//     return (
+//       <p style={{marginLeft: '5%'}}>{showComment}</p>
+//     )
+// }
   return (
-    <p style={{marginLeft: '5%'}}>{showComment}</p>
+    <>
+    {(playBackOn) &&
+      <div>
+      {items}
+      </div>
+    }
+    </>
   )
 }
